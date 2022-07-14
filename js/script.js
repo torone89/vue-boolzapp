1001fd3015c10a380c7e645d080d7095b189eaed
torone89 <torone89>
2022-07-14 17:53:41 +0200

    Milestone 1 Completata

--- js/script.js
+++ js/script.js
@@ -5,3 +5,59 @@ console.log("JS")
 // VERIFICA COLLEGAMENTO VUEJS
 
 console.log('Vue ok', Vue)
+
+new Vue({
+    el: '#root  ',
+    data: {
+        user: {
+            name: 'Nome Utente',
+            avatar: '_io'
+        },
+
+        contacts: [
+            {
+                name: 'Michele',
+                avatar: '_1',
+            },
+            {
+                name: 'Fabio',
+                avatar: '_2',
+            },
+            {
+                name: 'Samuele',
+                avatar: '_3',
+            },
+            {
+                name: 'Luisa',
+                avatar: '_4',
+            },
+            {
+                name: 'Michele',
+                avatar: '_1',
+            },
+            {
+                name: 'Fabio',
+                avatar: '_2',
+            },
+            {
+                name: 'Samuele',
+                avatar: '_3',
+            },
+            {
+                name: 'Luisa',
+                avatar: '_4',
+            }
+        ]
+
+    },
+    // Milestone 1 (DAY 1)
+    // // Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e
+    //  dall’interlocutore (bianco) assegnando due classi CSS diverse
+    methods: {
+        getAvatarImgProfile(contact) {
+            // FUNZIONE GENERAZIONE AVATARIMG
+            return `img/avatar${contact.avatar}.jpg`;
+        },
+
+    }
+});

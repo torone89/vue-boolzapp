f9974a68e9a9efa93c51ed0a426cf1b1c31b659b
torone89 <torone89>
2022-07-15 13:08:47 +0200

    Milestone 2 Completata

--- js/script.js
+++ js/script.js
@@ -199,11 +199,6 @@ new Vue({
     }
 });
 
-// Milestone 2
-// // Visualizzazione dinamica dei messaggi: tramite la direttiva v -for,
-//  visualizzare tutti i messaggi relativi al contatto attivo 
-//  all’interno del pannello della conversazione
-// // Click sul contatto mostra la conversazione del contatto cliccato
 
 
 

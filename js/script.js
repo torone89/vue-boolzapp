472fda191c8cdf1241702fed79f0d52c49391b47
torone89 <torone89>
2022-07-15 11:46:18 +0200

    Milestone 2 ( da fix)

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ console.log("JS")
 // VERIFICA COLLEGAMENTO VUEJS
 
 console.log('Vue ok', Vue)
+// AGGIUNGO PLUGIN
+dayjs.extend(dayjs_plugin_customParseFormat);
 
 new Vue({
     el: '#root  ',
@@ -13,41 +15,168 @@ new Vue({
             name: 'Nome Utente',
             avatar: '_io'
         },
-
+        currentIndex: 0,
         contacts: [
             {
                 name: 'Michele',
                 avatar: '_1',
+                visible: true,
+                messages: [{
+                    date: '10/01/2020 15:30:55',
+                    text: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 15:50:00',
+                    text: 'Ricordati di dargli da mangiare',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 16:15:22',
+                    text: 'Tutto fatto!',
+                    status: 'received'
+                }
+                ],
             },
             {
                 name: 'Fabio',
                 avatar: '_2',
+                visible: true,
+                messages: [{
+                    date: '20/03/2020 16:30:00',
+                    text: 'Ciao come stai?',
+                    status: 'sent'
+                },
+                {
+                    date: '20/03/2020 16:30:55',
+                    text: 'Bene grazie! Stasera ci vediamo?',
+                    status: 'received'
+                },
+                {
+                    date: '20/03/2020 16:35:00',
+                    text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                    status: 'received'
+                }
+                ],
             },
             {
                 name: 'Samuele',
                 avatar: '_3',
+                visible: true,
+                messages: [{
+                    date: '28/03/2020 10:10:40',
+                    text: 'La Marianna va in campagna',
+                    status: 'received'
+                },
+                {
+                    date: '28/03/2020 10:20:10',
+                    text: 'Sicuro di non aver sbagliato chat?',
+                    status: 'sent'
+                },
+                {
+                    date: '28/03/2020 16:15:22',
+                    text: 'Ah scusa!',
+                    status: 'received'
+                }
+                ],
             },
             {
                 name: 'Luisa',
                 avatar: '_4',
+                visible: true,
+                messages: [{
+                    date: '10/01/2020 15:30:55',
+                    text: 'Lo sai che ha aperto una nuova pizzeria?',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 15:50:00',
+                    text: 'Si, ma preferirei andare al cinema',
+                    status: 'received'
+                }
+                ],
             },
             {
                 name: 'Michele',
                 avatar: '_1',
+                visible: true,
+                messages: [{
+                    date: '10/01/2020 15:30:55',
+                    text: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 15:50:00',
+                    text: 'Ricordati di dargli da mangiare',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 16:15:22',
+                    text: 'Tutto fatto!',
+                    status: 'received'
+                }
+                ],
             },
             {
                 name: 'Fabio',
                 avatar: '_2',
+                visible: true,
+                messages: [{
+                    date: '20/03/2020 16:30:00',
+                    text: 'Ciao come stai?',
+                    status: 'sent'
+                },
+                {
+                    date: '20/03/2020 16:30:55',
+                    text: 'Bene grazie! Stasera ci vediamo?',
+                    status: 'received'
+                },
+                {
+                    date: '20/03/2020 16:35:00',
+                    text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                    status: 'received'
+                }
+                ],
             },
             {
                 name: 'Samuele',
                 avatar: '_3',
+                visible: true,
+                messages: [{
+                    date: '28/03/2020 10:10:40',
+                    text: 'La Marianna va in campagna',
+                    status: 'received'
+                },
+                {
+                    date: '28/03/2020 10:20:10',
+                    text: 'Sicuro di non aver sbagliato chat?',
+                    status: 'sent'
+                },
+                {
+                    date: '28/03/2020 16:15:22',
+                    text: 'Ah scusa!',
+                    status: 'received'
+                }
+                ],
             },
             {
                 name: 'Luisa',
                 avatar: '_4',
-            }
+                visible: true,
+                messages: [{
+                    date: '10/01/2020 15:30:55',
+                    text: 'Lo sai che ha aperto una nuova pizzeria?',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 15:50:00',
+                    text: 'Si, ma preferirei andare al cinema',
+                    status: 'received'
+                }
+                ],
+            },
         ]
+
 
     },
     // Milestone 1 (DAY 1)
@@ -59,5 +188,22 @@ new Vue({
             return `img/avatar${contact.avatar}.jpg`;
         },
 
+        setCurrentContact(index) {
+            this.currentIndex = index;          // method che setta il currentIndex 
+
+        },
+        // Funzione per Formattare la Data
+        printDate() {
+            return dayjs(this.date).format('dddd, DD/MM/YYYY HH:mm:ss')
+        }
     }
 });
+
+// Milestone 2
+// // Visualizzazione dinamica dei messaggi: tramite la direttiva v -for,
+//  visualizzare tutti i messaggi relativi al contatto attivo 
+//  all’interno del pannello della conversazione
+// // Click sul contatto mostra la conversazione del contatto cliccato
+
+
+
